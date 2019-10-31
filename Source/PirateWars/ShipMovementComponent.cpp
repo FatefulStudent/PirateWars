80257ef510e7ecfb9ba9758e2b41143c13d1d407
Vlad Ivanov <vladislavvladislavovich@gmail.com>
2019-10-31 18:05:17 +0300

    FINALLY created a valid movement component for my ship

--- Source/PirateWars/ShipMovementComponent.cpp
+++ Source/PirateWars/ShipMovementComponent.cpp
@@ -7,7 +7,6 @@
 
 UShipMovementComponent::UShipMovementComponent()
 {
-	MaxSpeed = 50000.f;
 }
 
 void UShipMovementComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
@@ -39,37 +38,7 @@ void UShipMovementComponent::TickComponent(float DeltaTime, enum ELevelTick Tick
 			Velocity = Velocity.GetUnsafeNormal() * MaxSpeed;
 		}
 
-		LimitWorldBounds();
 		bPositionCorrected = false;
-
-		// Move actor
-		FVector Delta = Velocity * DeltaTime;
-		UE_LOG(LogTemp, Warning, TEXT("Velocity: (%f, %f, %f)"), Velocity.X, Velocity.Y, Velocity.Z)
-		UE_LOG(LogTemp, Warning, TEXT("Delta: (%f, %f, %f)"), Delta.X, Delta.Y, Delta.Z)
-		if (!Delta.IsNearlyZero(1e-6f))
-		{
-			UE_LOG(LogTemp, Warning, TEXT("Delta is not zero"))
-			const FVector OldLocation = UpdatedComponent->GetComponentLocation();
-			const FQuat Rotation = UpdatedComponent->GetComponentQuat();
-
-			FHitResult Hit(1.f);
-			SafeMoveUpdatedComponent(Delta, Rotation, true, Hit);
-
-			if (Hit.IsValidBlockingHit())
-			{
-				HandleImpact(Hit, DeltaTime, Delta);
-				// Try to slide the remaining distance along the surface.
-				SlideAlongSurface(Delta, 1.f - Hit.Time, Hit.Normal, Hit, true);
-			}
-
-			// Update velocity
-			// We don't want position changes to vastly reverse our direction (which can happen due to penetration fixups etc)
-			if (!bPositionCorrected)
-			{
-				const FVector NewLocation = UpdatedComponent->GetComponentLocation();
-				Velocity = ((NewLocation - OldLocation) / DeltaTime);
-			}
-		}
 		
 		// Finalize
 		UpdateComponentVelocity();
@@ -78,45 +47,20 @@ void UShipMovementComponent::TickComponent(float DeltaTime, enum ELevelTick Tick
 
 void UShipMovementComponent::ApplyControlInputToVelocity(float DeltaTime)
 {
-	const FVector ControlAcceleration = GetPendingInputVector().GetClampedToMaxSize(1.f);
-	UE_LOG(LogTemp, Warning, TEXT("ControlAcceleration: (%f, %f, %f)"), ControlAcceleration.X, ControlAcceleration.Y, ControlAcceleration.Z)
-	const float AnalogInputModifier = (ControlAcceleration.SizeSquared() > 0.f ? ControlAcceleration.Size() : 0.f);
-	const float MaxPawnSpeed = GetMaxSpeed() * AnalogInputModifier;
-	const bool bExceedingMaxSpeed = IsExceedingMaxSpeed(MaxPawnSpeed);
+	const FVector ControlAcceleration = GetPendingInputVector().GetSafeNormal();
 
-	if (AnalogInputModifier > 0.f && !bExceedingMaxSpeed)
-	{
-		// Apply change in velocity direction
-		UE_LOG(LogTemp, Warning, TEXT("Apply change in velocity direction"))
-		if (Velocity.SizeSquared() > 0.f)
-		{
-			// Change direction faster than only using acceleration, but never increase velocity magnitude.
-			const float TimeScale = FMath::Clamp(DeltaTime * TurningBoost, 0.f, 1.f);
-			Velocity = Velocity + (ControlAcceleration * Velocity.Size() - Velocity) * TimeScale;
-		}
-	}
-	else
-	{
-		// Dampen velocity magnitude based on deceleration.
-		UE_LOG(LogTemp, Warning, TEXT("Dampen velocity magnitude based on deceleration"))
-		if (Velocity.SizeSquared() > 0.f)
-		{
-			const FVector OldVelocity = Velocity;
-			const float VelSize = FMath::Max(Velocity.Size() - FMath::Abs(Deceleration) * DeltaTime, 0.f);
-			Velocity = Velocity.GetSafeNormal() * VelSize;
-
-			// Don't allow braking to lower us below max speed if we started above it.
-			if (bExceedingMaxSpeed && Velocity.SizeSquared() < FMath::Square(MaxPawnSpeed))
-			{
-				Velocity = OldVelocity.GetSafeNormal() * MaxPawnSpeed;
-			}
-		}
-	}
-
-	// Apply acceleration and clamp velocity magnitude.
-	const float NewMaxSpeed = (IsExceedingMaxSpeed(MaxPawnSpeed)) ? Velocity.Size() : MaxPawnSpeed;
-	Velocity += ControlAcceleration * FMath::Abs(Acceleration) * DeltaTime;
-	Velocity = Velocity.GetClampedToMaxSize(NewMaxSpeed);
-	UE_LOG(LogTemp, Warning, TEXT("VeLoCiTy: (%f, %f, %f)"), Velocity.X, Velocity.Y, Velocity.Z)
+	// Update Velocity
+	Velocity = ControlAcceleration * GetMaxSpeed();
 	ConsumeInputVector();
+
+	// Rotate pawn
+	if (!ControlAcceleration.IsNearlyZero())
+	{
+		const FRotator& MovementAngle = ControlAcceleration.Rotation();
+		if (MovementAngle.Yaw != PawnOwner->GetActorRotation().Yaw)
+		{
+			// Turn in one frame
+			PawnOwner->SetActorRotation(MovementAngle);
+		}
+	}
 }
